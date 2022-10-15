e43742b1b7f3c0c638686beb4ea2f1347a3b6c3c
kobyrthr-dev <kobyrthr@gmail.com>
2022-10-15 19:45:54 -0400

    added row values

--- src/components/Mtable.js
+++ src/components/Mtable.js
@@ -1,10 +1,12 @@
 import { faker } from '@faker-js/faker';
+import { DataGrid } from '@mui/x-data-grid';
 
 let USERS = [];
 let STATUSES =['Active','Pending','Blocked'];
 
 for(let i=0;i<14;i++){
     USERS[i]={
+        id:i+1,
         name:faker.name.fullName(),
         email:faker.internet.email(),
         phone:faker.phone.number('501-###-###'),
@@ -15,9 +17,29 @@ for(let i=0;i<14;i++){
     }
 }
 console.log(USERS)
+
+const columns = [
+    { field: 'name', headerName: 'Name', width: 200 },
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'phone', headerName: 'Phone', width: 200 },
+    { field: 'jobTitle', headerName: 'Job Title',width: 200},
+    { field: 'company', headerName: 'Company',width: 200},
+    { field: 'status', headerName: 'Status',width: 200},
+  ];
+
+
+
 function MTable() {
     return(
-        'Table-x'
+        <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={USERS}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
     )
 }
 
